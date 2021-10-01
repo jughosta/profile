06d5f83f4567824ebe750c1af9c64239ce349b11
Julia Rechkunova <julia.rechkunova@gmail.com>
2021-10-01 09:05:29 +0200

    Switch to Next.js

--- components/SocialLink/SocialLink.tsx
+++ components/SocialLink/SocialLink.tsx
@@ -8,25 +8,9 @@ interface IProps {
   }
 }
 
-declare global {
-  interface Window {
-    ga?: (command: string, type: string, action: string, value: string) => void
-  }
-}
-
 const SocialLink: React.FC<IProps> = ({ name, url, icon }) => {
-  const onClick = () => {
-    window.ga?.('send', 'event', 'click', name)
-  }
-
   return (
-    <a
-      href={url}
-      title={name}
-      target="_blank"
-      rel="noopener noreferrer"
-      onClick={onClick}
-    >
+    <a href={url} title={name} target="_blank" rel="noopener noreferrer">
       <img src={icon.src} alt={name} className="h-5 w-5" />
     </a>
   )
